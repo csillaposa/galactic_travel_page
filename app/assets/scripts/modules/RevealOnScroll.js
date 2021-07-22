12e515398e28efb39363150f0d4021ca6d8b8b73
Csilla Pósa <posa.csilla@gmail.com>
2021-07-22 16:47:32 +0200

    Calculation for Reveal on Scroll

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -14,7 +14,10 @@ class RevealOnScroll {
     }
 
     calculateIfScrolledTo(el) {
-        console.log(el.getBoundingClientRect().y);
+        let scrollPercent = (el.getBoundingClientRect().y / window.innerHeight) * 100;
+        if (scrollPercent < 75) {
+            el.classList.add("reveal-item--is-visible");
+        }
     }
 
     hideInitially() {
