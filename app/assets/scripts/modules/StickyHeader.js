5c0164228f542c2fe6286601aa0b05331b891587
Csilla Pósa <posa.csilla@gmail.com>
2021-07-24 21:24:51 +0200

    JS for Sticky Header
    Created code for the sticky header, so when we scroll up or down, the current part of the page is highlighted in the nav bar.

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,6 +1,44 @@
+import { throttle } from "lodash";
+import { debounce } from "lodash";
+
 class StickyHeader {
     constructor() {
-        
+        this.pageSections = document.querySelectorAll(".page-section");
+        this.browserHeight = window.innerHeight;
+        this.previousScrollY = window.scrollY;
+        this.events();
+    }
+
+    events() {
+        window.addEventListener("scroll", throttle(() => this.runOnScroll(), 200));
+        window.addEventListener("resize", debounce(() => {
+            this.browserHeight = window.innerHeight;
+        }, 333));
+    }
+
+    runOnScroll() {
+        this.determineScrollDirection();
+        this.pageSections.forEach(el => this.calcSection(el));
+    }
+
+    determineScrollDirection() {
+        if (window.scrollY > this.previousScrollY) {
+            this.scrollDirection = 'down';
+        } else {
+            this.scrollDirection = 'up';
+        }
+        this.previousScrollY = window.scrollY;
+    }
+
+    calcSection(el) {
+        if (window.scrollY + this.browserHeight > el.offsetTop && window.scrollY < el.offsetTop + el.offsetHeight) {
+            let scrollPercent = el.getBoundingClientRect().y / this.browserHeight * 100;
+            if (scrollPercent < 35 && scrollPercent > -0.1 && this.scrollDirection == 'down' || scrollPercent < 35 && this.scrollDirection == 'up') {
+                let matchingLink = el.getAttribute("data-matching-link");
+                document.querySelectorAll(`.site-header__nav a:not(${matchingLink})`).forEach(el => el.classList.remove("is-current-link"));
+                document.querySelector(matchingLink).classList.add("is-current-link");
+            }
+        }
     }
 }
 
