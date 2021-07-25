f5e70d6abad843ea5e55e17967b11cf6d56348f0
Csilla Pósa <posa.csilla@gmail.com>
2021-07-25 15:23:02 +0200

    Updated cloud function to check password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,26 @@
+//it is a CLOUD FUNCTION, because it is executed somewhere in the cloud
+
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to the super secret area"
-    })
+    //here we can perform server side actions
+    const secretContent = `
+    <h3>Welcome to the secret area</h3>
+    `
+    //we have body in POSTMAN
+    let body;
+    if (event.body) {
+        body = JSON.parse(event.body)
+    } else {
+        body = {}
+    }
+
+    if (body.password == "javascript") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        })
+    } else {
+        callback(null, {
+            statusCode: 401
+        })
+    }
 }
