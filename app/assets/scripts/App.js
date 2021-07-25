dc2ff3d2d4a1afa16dd4e7b74388522a50a3f070
Csilla Pósa <posa.csilla@gmail.com>
2021-07-25 09:38:37 +0200

    Code splitting for Modal
    To be able to load JS code only if necessary.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,14 +1,31 @@
 import '../styles/styles.css';
 import BurgerMenu from './modules/BurgerMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
-import Modal from './modules/Modal';
 import StickyHeader from './modules/StickyHeader';
 
 new BurgerMenu();
 new RevealOnScroll(document.querySelectorAll(".features__item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonials__item"), 60);
-new Modal();
 new StickyHeader();
+
+let modal;
+
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+        e.preventDefault();
+        //promise
+        //if the promise is returned, then() is called, where we create the new instance of the object
+        //if there is a problem, catch() is called
+        if (typeof modal == "undefined") {
+            import('./modules/Modal').then(x => {
+                modal = new x.default();
+                setTimeout(() => modal.openTheModal(), 20);
+            }).catch(() => console.log("There was a problem."));
+        } else {
+            modal.openTheModal();
+        }
+    })
+})
 
 if(module.hot) {
     module.hot.accept()
